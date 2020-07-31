038d06ac02200f7dde2e8df30e4cfec29137cafd
Lucashylario <lucashyfrotend@hotmail.com>
2020-07-31 15:01:04 -0300

    assas

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'https://localhost:8080/categorias'
-  : 'https://viniflix-alura.herokuapp.com/categorias';
+  : 'https://hylarioflix.herokuapp.com/categorias';
 
 export default {
-  URL,
+  URL
 };
